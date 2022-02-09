401eaaa8306c1042d2ac9e022dd6c6a83ff0d94d
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-09 14:38:51 +0300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "INPUT_PATH: ${INPUT_PATH}"
+echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
+echo "INPUT_ARGS: ${INPUT_ARGS}"
+
+if [[ -z "${INPUT_PATH}" ]]; then
+  INPUT_PATH="."
+fi
+
 CodeWeTrustRunner ${INPUT_PATH} --product-name ${GITHUB_REPOSITORY} --PipelineSettings:LicenseKey ${INPUT_LICENSEKEY} ${INPUT_ARGS}
