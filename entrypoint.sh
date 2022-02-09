6d50be066578050e3d55720ac456faa7478fb5a7
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-09 13:46:10 +0300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
-set -e
+#!/bin/sh
 
 ./CodeWeTrustRunner ${INPUT_PATH} --product-name ${{ github.event.repository.name }} --PipelineSettings:LicenseKey ${INPUT_LICENSEKEY} ${INPUT_ARGS}
