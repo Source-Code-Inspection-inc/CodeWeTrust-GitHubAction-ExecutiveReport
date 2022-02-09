c16498c87425327ab1b83dd53d96e5b0e4558f72
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-09 14:48:40 +0300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ echo "INPUT_PATH: ${INPUT_PATH}"
 echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
 echo "INPUT_ARGS: ${INPUT_ARGS}"
 
+if [[ -z "${INPUT_LICENSEKEY}" ]]; then
+  echo "License key is not set. Check licenseKey action parameter."
+  exit 1
+fi
+
 if [[ -z "${INPUT_PATH}" ]]; then
   INPUT_PATH="."
 fi
