d62101f2ba8450ba8b022e6618ae0b77b5f7d4f3
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-02-09 19:47:27 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,6 @@ if [[ -z "${INPUT_PATH}" ]]; then
   INPUT_PATH="."
 fi
 
-echo "Command: CodeWeTrustRunner ${INPUT_PATH} --product-name ${GITHUB_REPOSITORY} --PipelineSettings:LicenseKey ${INPUT_LICENSEKEY} ${INPUT_ARGS}"
+echo "Command: CodeWeTrustRunner ${INPUT_PATH} --product-name ${GITHUB_REPOSITORY} --PipelineSettings:LicenseKey ${INPUT_LICENSEKEY} --PipelineSettings:WriteExecutiveReport true ${INPUT_ARGS}"
 
 CodeWeTrustRunner ${INPUT_PATH} --product-name ${GITHUB_REPOSITORY} --PipelineSettings:LicenseKey ${INPUT_LICENSEKEY} --PipelineSettings:WriteExecutiveReport true ${INPUT_ARGS}
